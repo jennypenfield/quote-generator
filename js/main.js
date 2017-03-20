0bf26c7c32df97ea20b3eae06a74b16a68e72788
jennypenfield <jipenfield11@gmail.com>
2017-03-19 20:43:05 -0500

    committed files for random quote generator

---
+++ js/main.js
@@ -0,0 +1,27 @@
+var forismaticAPI = 'http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?'
+
+var currentQuote = ''
+var currentAuthor = ''
+
+$(document).ready(function () {
+  $.getJSON(forismaticAPI, getQuote, 'jsonp')
+})
+
+var getQuote = function (data) {
+  $('#quote').text(data.quoteText)
+  currentQuote = data.quoteText
+
+  if (data.quoteAuthor === '') {
+    data.quoteAuthor = 'Unknown'
+  }
+  $('#author').text('~ ' + data.quoteAuthor)
+  currentAuthor = data.quoteAuthor
+
+  $('#tweet-quote').on('click', function () {
+    window.open('https://twitter.com/intent/tweet?text=' + '"' + currentQuote + '  -' + currentAuthor)
+  })
+}
+
+$('#get-quote').on('click', function () {
+  $.getJSON(forismaticAPI, getQuote, 'jsonp')
+})
